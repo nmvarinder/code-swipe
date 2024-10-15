9895e22cfc32fa8ab76e7bfcf17cf2a9327a9658
varinder <nmvarinder@gmail.com>
2024-10-15 20:24:53 +0530

    chapter 17: routing and request handler

---
+++ chapters.md
@@ -0,0 +1,23 @@
+Chapter: 16: CREATING OUR EXPRESS SERVER
+-create a repo
+-Initialize a repo
+-node_modules, package.json, package-lock.json
+-install express
+-create a server
+-listen to port 
+-write req handler for /test and more router api
+-install nodemon and config script in package.json
+-diff. bet dependency vs dev-dependency
+-version precedence like caret(^), tilde(~) or none
+-what is the use of -g while npm install
+
+
+Chapter: 16: ROUTING AND REQUEST HANDLER
+-play with different router
+-try to implement /hello and /hello/2 and find some key points
+-order of router is very important and matter alots = specially in use method
+-install postman and make a get hello router request
+
+-must explore->  https://expressjs.com/en/guide/routing.html
+
+- diff bt req.params vs req.query
--- src/index.js
+++ src/index.js
@@ -2,17 +2,37 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello", (req, res) => {
-    res.send("hello world, welcome");
+
+/* // use method handle all type of http request, so any scenario of request weather its get, put, post, delete and others. use will works
+// better to avoid use method
+app.use('/users', (req,res) => {
+    res.send("jadu jo glt hai");
+}) */
+
+app.get('/', (req,res) => {
+    res.send("testing")
 })
 
-app.use("/", (req, res) => {
-    res.send("first page"); 
+app.get('/users', (req,res) => {
+    res.send("testing ordering in get method");
 })
 
-app.use("/test", (req, res) => {
-    res.send("hello from the server");
+app.get('/users/:userId', (req,res) => {
+    console.log(req.query);
+    console.log(req.params);
+    res.send({ firstName: "varinder", lastName: "chauhan"});
 })
+
+
+app.post('/users', (req,res) => {
+    res.send("data stored in db: successfully")
+})
+
+
+app.delete('/users', (req,res) => {
+    res.send("data deleted")
+})
+
 
 app.listen(3000, () => {
     console.log("server is successfully listening of port number: 3000");
